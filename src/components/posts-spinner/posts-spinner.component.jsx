7bd6a470450133d08bbbde5dfbe963abbe6a5d03
Joshmatjjen <joshmatparrot@gmail.com>
2020-04-13 06:54:21 +0100

    update on spinner

--- src/components/posts-spinner/posts-spinner.component.jsx
+++ src/components/posts-spinner/posts-spinner.component.jsx
@@ -24,7 +24,7 @@ const PostsSpinner = WrappedComponent => {
         }, 3000);
         setTimeout(() => {
           setErrorDIsplay("none");
-        }, 20000);
+        }, 2000);
       }
       // else if (postError === null) {
       //   setLoadBar(99.9);
