28c00d3cfdacd55f70920cecbdc151508d7e62b8
Ucheduk <dukweb4@gmail.com>
2020-06-16 12:53:48 +0100

    fix: netlify

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ import Highlight from './components/instagram/highlight/highlight.component';
 import Stories from './components/instagram/stories/stories.component';
 import InstagramPosts from './pages/instagram/posts.component';
 import YoutubeVideo from './pages/youtube/video.component';
-import TwitterVideo from './pages/twitter/video.component';
 import UserNamePosts from './pages/instagram/username-posts.component';
 import HashTagPosts from './pages/instagram/hashtag-posts.component';
 import HighlightPosts from './pages/instagram/highlight-posts.component';
@@ -75,7 +74,6 @@ const App = ({ checkUserSession, currentUser, token, location, history }) => {
         <Route exact path="/" component={Home} />
         <Route exact path="/instagram/posts" component={InstagramPosts} />
         <Route exact path="/youtube/video" component={YoutubeVideo} />
-        <Route exact path="/twitter/video" component={TwitterVideo} />
         <Route
           exact
           path="/instagram/post-by-username"
